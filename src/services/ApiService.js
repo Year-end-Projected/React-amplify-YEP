26db9d9f048ebd5dc873e4c8ae687818d56b9bc1
Stenalo <60882835+Stenalo@users.noreply.github.com>
2023-08-28 12:28:26 +0200

    Update ApiService.js

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,1 +1,23 @@
+import { URL_API } from "./Const";
 
+class ApiService {
+    constructor() {
+        this.state = {
+        };
+    }
+
+    async getPublications() {
+        var urlApi = URL_API + "publications";
+
+        try {
+            const response = await fetch(urlApi);
+            const data = await response.json();
+            this.data = data;
+        } catch (error) {
+            console.log(error);
+        }
+        return this.data;
+    }
+}
+
+export default ApiService;
