26225a9a1558e9db86bf952e1bc75b60a5b75c44
Stenalo <60882835+Stenalo@users.noreply.github.com>
2023-08-28 14:33:11 +0200

    Create MyPublications.js

---
+++ src/components/Publications/MyPublications.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+import './MyPublications.css';
+import My from "../Profile/MyProfile";
+
+
+const MyPublications = () => {
+    return (
+        <>
+           Mes Publications
+        </>
+    );
+};
+
+export default MyPublications;
